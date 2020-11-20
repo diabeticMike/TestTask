8c417027191541ff0a1f45b5b299e4ca367bfed3
diabeticMike <tmymichael@gmail.com>
2020-11-20 19:58:17 +0200

    add auth repo interface and realization

---
+++ repository/auth.go
@@ -0,0 +1,30 @@
+package repository
+
+import (
+	"database/sql"
+
+	"github.com/TestTask/model"
+)
+
+// NewAuthRepo return AuthRepository instance
+func NewAuthRepo(db *sql.DB) AuthRepository {
+	return &authRepo{db: db}
+}
+
+// AuthRepository interface for getting auth info from db
+type AuthRepository interface {
+	GetAuthByAPIKey(key string) (model.Auth, error)
+}
+
+type authRepo struct {
+	db *sql.DB
+}
+
+func (ar *authRepo) GetAuthByAPIKey(key string) (model.Auth, error) {
+	row := ar.db.QueryRow("SELECT * FROM auth WHERE api-key=$1 LIMIT 1;", key)
+	auth := model.Auth{}
+	if err := row.Scan(&key, &auth.APIKey); err != nil {
+		return auth, err
+	}
+	return auth, nil
+}
