a685df210172399292024507227bf5c49159f4a8
diabeticMike <tmymichael@gmail.com>
2020-11-20 19:59:57 +0200

    add comments to auth repo

--- repository/auth.go
+++ repository/auth.go
@@ -16,10 +16,12 @@ type AuthRepository interface {
 	GetAuthByAPIKey(key string) (model.Auth, error)
 }
 
+// AuthRepository realization using mysql
 type authRepo struct {
 	db *sql.DB
 }
 
+// GetAuthByAPIKey return all auth entity by api-key
 func (ar *authRepo) GetAuthByAPIKey(key string) (model.Auth, error) {
 	row := ar.db.QueryRow("SELECT * FROM auth WHERE api-key=$1 LIMIT 1;", key)
 	auth := model.Auth{}
